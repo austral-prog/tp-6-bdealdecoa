6ec2f831e0e4498f75f83123c8b6e8252d68e4c6
bdealdecoa <bdealdecoa@mail.austral.edu.ar>
2025-05-09 12:10:13 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,25 +1,14 @@
-
 def remove_elements(lst):
     # Elimina el primer (índice 0), quinto (índice 4) y sexto (índice 5) elementos, si existen
-    def remove_elements(lst):
-    lst = lst[:]  # Make a copy so the original list is not modified
-    # remove  6th element (index 5)
-    if len(lst) > 5:
-        del lst[5]
-    # remove  5th element (index 4)
-    if len(lst) > 4:
-        del lst[4]
-    # remove  1st element (index 0)
-    if len(lst) > 0:
-        del lst[0]
-    return lst
+    indices_to_remove = [0, 4, 5]
+    return [el for i, el in enumerate(lst) if i not in indices_to_remove]
 
 def add_elements(lst):
-    # add pink and yellow to the start and end of list
+    # Agrega 'Pink' al inicio y 'Yellow' al final
     return ['Pink'] + lst + ['Yellow']
 
 def is_empty(lst):
-    # return true if the list is empty
+    # Devuelve True si la lista está vacía
     return len(lst) == 0
 
 def check_lists(lst1, lst2):
@@ -28,12 +17,13 @@ def check_lists(lst1, lst2):
         return lst1[2] == lst2[2]
     return False
 
-
 def list_of_lists(lst_of_lsts):
-    return [
-        lst_of_lsts[0][:2],     # first 2 elements of the first list
-        lst_of_lsts[1][1:4],    # Elements from index 1 to 3 (4 is not included)
-        lst_of_lsts[2][-2:]     # Last 2 elements of the third list
-    ]
-
-
+    # Aplica recortes específicos a las 3 listas internas
+    modified = []
+    if len(lst_of_lsts) >= 1:
+        modified.append(lst_of_lsts[0][:2])  # Primeros 2 elementos
+    if len(lst_of_lsts) >= 2:
+        modified.append(lst_of_lsts[1][1:4])  # Elementos del índice 1 al 3
+    if len(lst_of_lsts) >= 3:
+        modified.append(lst_of_lsts[2][-2:])  # Últimos 2 elementos
+    return modified
